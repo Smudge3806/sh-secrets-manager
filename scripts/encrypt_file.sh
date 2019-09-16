a2195e110d522aa6dd5315cf2fda00dd60f76da4
Chris Smith <chris@xedi.com>
2019-09-16 11:34:39 +0100

    Made some linting fixes

--- scripts/encrypt_file.sh
+++ scripts/encrypt_file.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -z $2 ]]; then
-    read -s -p "Encryption Passcode: " pswd
+    read -r -s -p "Encryption Passcode: " pswd
 else
     pswd=$2
 fi
 
-echo $pswd | gpg --batch --yes --symmetric --cipher-algo AES256 --passphrase-fd 0 $1
+echo "${pswd}" | gpg --batch --yes --symmetric --cipher-algo AES256 --passphrase-fd 0 "${1}"
